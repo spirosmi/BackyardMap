93d13c35ef49568563b90e23bbfcb2a2b1760853
Sari Saba Sadiya <sadisari@gmail.com>
2020-10-19 17:55:45 -0400

    Update scripts.js

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -44,9 +44,9 @@
     // Collapse Navbar
     var navbarCollapse = function () {
         if ($("#mainNav").offset().top > 100) {
-            //$("#mainNav").addClass("navbar-shrink");
+            $("#mainNav").addClass("navbar-shrink");
         } else {
-            $("#mainNav").removeClass("navbar-shrink");
+            //$("#mainNav").removeClass("navbar-shrink");
         }
     };
     // Collapse now if page is not at top
