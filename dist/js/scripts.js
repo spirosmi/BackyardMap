adf2dfb8850540083ed5983f41d4ce25fc88815d
Sari Saba Sadiya <sadisari@gmail.com>
2020-10-22 15:06:30 -0400

    Update scripts.js

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -50,7 +50,7 @@
         }
     };
     // Collapse now if page is not at top
-    navbarCollapse();
+    // navbarCollapse();
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
